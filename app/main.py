3f06e55d7ed04f2714736d6a4bb1d21c5f54eacf
mac <mac@macs-MacBook-Pro.local>
2025-10-02 12:47:02 -0400

    fix rutas flask

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
-import os
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -8,22 +8,22 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         url = request.form.get("url")
-        if url:
-            try:
-                # Descargar video desde la URL
-                response = requests.get(url, stream=True)
-                save_path = os.path.join("app", "static", "video.mp4")
-                with open(save_path, "wb") as f:
-                    for chunk in response.iter_content(chunk_size=1024):
-                        if chunk:
-                            f.write(chunk)
-                return "✅ Video descargado correctamente en static/video.mp4"
-            except Exception as e:
-                return f"❌ Ocurrió un error: {e}"
+        if not url:
+            return "❌ No se recibió ningún enlace"
+
+        try:
+            response = requests.get(url, stream=True)
+            save_path = os.path.join("app/static", "video.mp4")
+            with open(save_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            return "✅ Video descargado correctamente"
+        except Exception as e:
+            return f"⚠️ Ocurrió un error: {e}"
+
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
 
 
 
